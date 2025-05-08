7abc4ecbcac61df7bb8b59c9bbbafb744b0ec9fa
Aayush Batham <aayushbatham33@gmail.com>
2025-05-08 23:01:40 +0530

    test commit

--- server/src/main/java/com/server/ServerApplication.java
+++ server/src/main/java/com/server/ServerApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args ) {
+		
+		System.out.println("Server is running...");
+		System.out.println("Server started on port 8080");
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
